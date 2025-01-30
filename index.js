7859b548699fcfca147d782ecc0da855ad6e9a31
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-30 23:45:27 +0600

    GET Operation for Testing API Completed

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+app.get("test-ai", async (req, res) => {
+    const prompt = "Explain how AI works";
+
+    const result = await model.generateContent(prompt);
+})
 
 app.get("/", (req, res) => {
   res.send("Let's Crack The Power of AI");
