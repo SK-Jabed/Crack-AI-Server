ab1fd733f96a9925217d1ce7317b090ca5d52dc0
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-30 23:00:52 +0600

    Crack AI Server Side Repository Added

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Let's Crack The Power of AI");
+});
+
+app.listen(port, () => {
+  console.log(`Crack AI Server is running on port: ${port}`);
+});
